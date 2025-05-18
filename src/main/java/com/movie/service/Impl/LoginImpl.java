3f090e8cea2474c01c52b3d2af2c81c840955593
GhostSharksDog <1766505342@qq.com>
2025-05-18 14:02:01 +0800

    能跑

---
+++ src/main/java/com/movie/service/Impl/LoginImpl.java
@@ -0,0 +1,8 @@
+package com.movie.service.Impl;
+
+import com.movie.service.LoginService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginImpl implements LoginService {
+}
