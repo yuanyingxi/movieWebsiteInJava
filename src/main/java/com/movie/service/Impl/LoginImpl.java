9ca771e1bd8f9aad70ed684e642ddfe95337b408
GhostSharksDog <1766505342@qq.com>
2025-05-19 17:09:26 +0800

    reneme Impl

--- src/main/java/com/movie/service/Impl/LoginImpl.java
+++
@@ -1,8 +0,0 @@
-package com.movie.service.Impl;
-
-import com.movie.service.LoginService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class LoginImpl implements LoginService {
-}
