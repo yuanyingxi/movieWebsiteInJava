18f657a449bb2632c4e5d2088abca908bbb044f8
luo20236 <20232005044@m.scnu.edu.cn>
2025-06-17 23:37:25 +0800

    主创作品

---
+++ src/main/java/com/movie/controller/CreatorMovieController.java
@@ -0,0 +1,32 @@
+package com.movie.controller;
+
+import com.movie.common.Result;
+import com.movie.entity.CreatorMovie;
+import com.movie.entity.Movie;
+import com.movie.entity.MovieVO;
+import com.movie.mapper.CreatorMovieMapper;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/creatorMovie")
+public class CreatorMovieController {
+    @Autowired
+    private CreatorMovieMapper creatorMovieMapper;
+
+
+    @GetMapping
+    public Result creatorMovie(
+     @RequestParam(defaultValue = "1")Integer creator_id
+
+    ){
+
+        List<CreatorMovie> movies = creatorMovieMapper.findMoviesByCreatorId(creator_id);
+
+        return Result.success(movies);
+    }
+}
